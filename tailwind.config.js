2660059a1882a8572e275a3c5b19b0e9b38bcdde
Aganepes <aaganepes@gmail.com>
2025-12-07 22:38:11 +0500

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,13 @@ module.exports = {
   darkMode: "class",
   theme: {
     container: {
-      center: true,
-      padding: "16px",
     },
     extend: {
       colors: {
-        primary: "#14b8a6",
-        secondary: "#64748b",
-        neutral: "#f3f4f6",
-        vercel: "#0284c7",
-        netlify: "#2563eb",
-        github: "#57534e",
-        coffee: "#d97706",
-        kofi: "#db2777",
-        dark: "#0f172a",
+        primary: "#27cbc2",
+        secondary: "#218580",
       },
       screens: {
-        xl: "1320px",
       },
     },
   },
